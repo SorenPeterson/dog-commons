28f8bc25b24b0ba22d64f390b2dcd18f23b2f387
Soren Peterson <soren468@gmail.com>
2015-06-04 12:19:05 -0700

    doge is on the map

--- app/client/routes.js
+++ app/client/routes.js
@@ -7,9 +7,9 @@ Router.route('/home');
 Router.route('/map', function() {
 	GoogleMaps.ready('mainMap', function(map) {
 		var overlayBounds = new google.maps.LatLngBounds(
-			new google.maps.LatLng(40.712216, -74.22655),
-			new google.maps.LatLng(40.773941, -74.12544));
-		var mapOverlay = new google.maps.GroundOverlay('https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg', overlayBounds);
+			new google.maps.LatLng(44.85, -93.632),
+			new google.maps.LatLng(44.87, -93.597));
+		var mapOverlay = new google.maps.GroundOverlay('http://otterhive.com/img/backgrounds/doge.gif', overlayBounds);
 		mapOverlay.setMap(map.instance);
 	});
 	this.render('Map');
