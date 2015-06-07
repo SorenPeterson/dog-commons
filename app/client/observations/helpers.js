dadf83af7e9d148404d8fcdce978e3c8bebe8540
Soren Peterson <soren468@gmail.com>
2015-06-06 22:18:12 -0700

    Committing to pull back to master

--- app/client/observations/helpers.js
+++ app/client/observations/helpers.js
@@ -1,9 +1,22 @@
+var today = function() {
+	return moment(new Date).format('YYYYMMDD');
+}
+
 Template.Observations.helpers({
 	observations: function() {
-		var today = function() {
-			return moment(new Date).format('YYYYMMDD');
-		}
 		return Observations.find({date: today()});
 	}
 });
 
+Template.Observations.events({
+	'submit form': function(e, tmpl) {
+		e.preventDefault();
+		var textArea = tmpl.find('textarea');
+		if(Observations.find({date: today()}).count < 3) {
+			Observations.insert({date: today(), content: textArea.value });
+		} else {
+			alert('no more today');
+		}
+		textArea.value = '';
+	}
+});
