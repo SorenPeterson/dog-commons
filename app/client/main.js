4879b9eb91aab985702fa6cf665ef2b138b5358e
Soren Peterson <soren468@gmail.com>
2015-06-09 10:06:09 -0700

    putting everything into one file

--- app/client/main.js
+++ app/client/main.js
@@ -22,3 +22,27 @@ if(Meteor.isCordova) {
 		debug: false
 	});
 }
+
+Router.onBeforeAction(function() {
+	this.layout('Layout');
+	this.next();
+});
+
+Router.route('/', function() {
+	this.render('Splash');
+});
+
+Router.route('/home');
+
+Router.route('/map', function() {
+	GoogleMaps.ready('mainMap', function(map) {
+		var overlayBounds = new google.maps.LatLngBounds(
+			new google.maps.LatLng(44.85, -93.632),
+			new google.maps.LatLng(44.87, -93.597));
+		var mapOverlay = new google.maps.GroundOverlay('http://otterhive.com/img/backgrounds/doge.gif', overlayBounds);
+		mapOverlay.setMap(map.instance);
+	});
+	this.render('Map');
+});
+
+Router.route('/observations');
