7bfc0bdcd52bc20b64d255ac75c6ba98a4f57112
Soren Peterson <soren468@gmail.com>
2015-06-09 15:04:23 -0700

    Add meteor method to grab facebook page data

--- app/server/main.js
+++ app/server/main.js
@@ -1,1 +1,19 @@
+Meteor.startup(function() {
+	global.Future = Npm.require('fibers/future');
+});
 
+Meteor.methods({
+	facebook_feed: function() {
+		var fut = new Future();
+
+		var url = 'https://graph.facebook.com/oauth/access_token?client_id=' + process.env.FB_APP_ID + '&client_secret=' + process.env.FB_APP_SECRET + '&grant_type=client_credentials'
+		HTTP.get(url, {}, function(notreallysurewhatthisis, response) {
+			console.log(response.content);
+			HTTP.get('https://graph.facebook.com/MnArboretum/feed?' + response.content, function(dogeroni, response) {
+				fut.return(response.content);
+			});
+		});
+		
+		return fut.wait();
+	}
+});
