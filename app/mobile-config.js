960b332a37187d0b85054d6f1284c918bd4ec073
Soren Peterson <soren468@gmail.com>
2015-06-27 17:03:44 -0500

    remove some tgz builds and access rules from mobile config

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,10 +1,8 @@
 App.info({
-	name: 'nbt',
-	description: 'doge doge doge',
-	version: '0.0.1'
+	name: '3030 - Nature Based Therapy',
+	description: 'Minnesota Landscape Arboretum',
+	version: '0.0.2'
 });
 
-App.accessRule('*.google.com/*');
-App.accessRule('*.googleapis.com/*');
-App.accessRule('*.gstatic.com/*');
 App.accessRule('*.otterhive.com/*');
+
