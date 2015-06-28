824bb49a6007197d11df787e2c03cc18f3352641
Soren Peterson <soren468@gmail.com>
2015-06-28 02:56:58 -0500

    remove ipad icon from config

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -1,12 +1,11 @@
 App.info({
 	name: '3030 - Nature Based Therapy',
 	description: 'Minnesota Landscape Arboretum',
-	version: '0.0.3'
+	version: '0.0.2'
 });
 
 App.icons({
-	'iphone': 'icons/icon-57.png',
-	'ipad': 'icons/icon-72.png'
+	'iphone': 'icons/icon-57.png'
 });
 
 App.accessRule('*.otterhive.com/*');
