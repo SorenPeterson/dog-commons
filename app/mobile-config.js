c950275f47d08bdc40b1712345ec697091e9575f
Soren Peterson <soren468@gmail.com>
2015-06-28 02:15:49 -0500

    list icons in mobile config

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -4,6 +4,11 @@ App.info({
 	version: '0.0.2'
 });
 
+App.icons({
+	'iphone': 'icons/icon-60.png',
+	'iphone_2x': 'icons/icon-60@2x.png'
+});
+
 App.accessRule('*.otterhive.com/*');
 App.accessRule('192.168.1.131/*');
 App.accessRule('localhost/*');
