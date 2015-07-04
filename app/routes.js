45150efda6ed6deabf125117f60c72832d3e50f7
Soren Peterson <soren468@gmail.com>
2015-07-03 19:37:59 -0500

    move server geolocation route into main routes file

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ Router.onBeforeAction(function() {
 	console.log('hi');
 	this.next();
 }, {
-	except: ['splash', 'home']
+	except: ['splash', 'home', 'GeolocationBGRoute']
 });
 
 Router.route('/', function() {
@@ -75,4 +75,33 @@ Router.route('/identification', function() {
 //     uuid: Match.Optional(String),
 //     device: Match.Optional(String)
 //   }
+
+Router.route('/api/geolocation', function() {
+	// GET, POST, PUT, DELETE
+	var requestMethod = this.request.method;
+	// Data from a POST request
+	var requestData = this.request.body;
+
+	// log stuff
+	//console.log('GeolocationBG post: ' + requestMethod);
+	//console.log(JSON.stringify(requestData));
+
+	// TODO: security/validation
+	//  require some security with data
+	//  validate userId/uuid/etc (inside Meteor.call?)
+
+	// Can insert into a Collection from the server (or whatever)
+	if (GeolocationLog.insert(requestData)) {
+		this.response.writeHead(200, {'Content-Type': 'application/json'});
+		this.response.end('ok');
+		return;
+	}
+
+	// if we end up with an error case, you can return 500
+	this.response.writeHead(500, {'Content-Type': 'application/json'});
+	this.response.end('failure');
+}, {
+	where: 'server',
+	name: 'GeolocationBGRoute'
+});
 
