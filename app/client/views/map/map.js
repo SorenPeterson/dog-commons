582339fe70713a9cf62cc81465d38c4d6e583b32
Soren Peterson <soren468@gmail.com>
2015-07-10 15:10:37 -0500

    remove styles js because we're only usig one background now

--- app/client/views/map/map.js
+++ app/client/views/map/map.js
@@ -2,3 +2,12 @@ Template.Map.onRendered(function() {
 	$('#map').height(window.innerHeight - 60);
 });
 
+Template.Map.onRendered(function() {
+	this.autorun(function() {
+		if(Mapbox.loaded()) {
+			L.mapbox.accessToken = 'pk.eyJ1Ijoic29yZW40NjgiLCJhIjoiOTZiMTc3MThlZjFlMTFmOGQ0NjIwNzcwODJhZTM4YWQifQ.Ycl3ox-mvLD4IM64nGNCxA';
+			window.map = L.mapbox.map('map', 'soren468.2db88a2d');
+		}
+	});
+});
+
