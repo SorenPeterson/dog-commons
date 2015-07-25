f5391633d8da4afc4c45cc2fd95e895f73b0eca3
Soren Peterson <soren468@gmail.com>
2015-07-25 17:35:10 -0500

    Using styled map designed in mapbox studio

--- app/client/views/map/map.js
+++ app/client/views/map/map.js
@@ -6,7 +6,9 @@ Template.Map.onRendered(function() {
 	this.autorun(function() {
 		if(Mapbox.loaded()) {
 			L.mapbox.accessToken = 'pk.eyJ1Ijoic29yZW40NjgiLCJhIjoiOTZiMTc3MThlZjFlMTFmOGQ0NjIwNzcwODJhZTM4YWQifQ.Ycl3ox-mvLD4IM64nGNCxA';
-			window.map = L.mapbox.map('map', 'soren468.2db88a2d');
+			window.map = L.mapbox.map('map', 'soren468.774e7f1d');
+			var center = new L.LatLng(44.859180, -93.614343);
+			window.map.setView(center, 14);
 		}
 	});
 });
