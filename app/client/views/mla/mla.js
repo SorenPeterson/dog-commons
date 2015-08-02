c82e9e0c05ace41d0f2c7a880e3ecca8007db07c
Soren Peterson <soren468@gmail.com>
2015-08-02 15:54:09 -0500

    put headers in all necessary spots

--- app/client/views/mla/mla.js
+++ app/client/views/mla/mla.js
@@ -1,8 +1,10 @@
 Template.Mla.events({
 	'click a': function(evt, tmpl) {
-		evt.preventDefault();
 		var href = $(evt.target).closest('a').attr('href');
-		window.open(href, '_blank', 'location=yes');
+		if(href !== '/home') {
+			evt.preventDefault();
+			window.open(href, '_blank', 'location=yes');
+		}
 	}
 });
 
