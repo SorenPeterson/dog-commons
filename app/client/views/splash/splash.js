7e754f4c6feee0702c0c68f4292d0ea64078dc8d
Soren Peterson <soren468@gmail.com>
2015-08-01 19:38:35 -0500

    hide status bar on spolash page

--- app/client/views/splash/splash.js
+++ app/client/views/splash/splash.js
@@ -2,3 +2,12 @@ Template.Splash.onRendered(function() {
 	$('.splash h1').fitText(0.7);
 	$('.splash a').fitText(0.15);
 });
+
+if(Meteor.isCordova) {
+	Template.Splash.onRendered(function() {
+		StatusBar.hide();
+	});
+	Template.Splash.onDestroyed(function() {
+		StatusBar.show();
+	});
+}
