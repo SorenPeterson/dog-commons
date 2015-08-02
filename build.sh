7783251d63e04bca5a05def0eee55df3ee095bb3
Soren Peterson <soren.t.peterson@gmail.com>
2015-08-01 19:30:17 -0500

    modify build script to point to bogusip

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull origin master
 echo y | rm -rf devbuild/*
 cd app
-meteor build ../devbuild --server=http://nbt.otterhive.com
+meteor build ../devbuild --server=http://0.0.0.0:80/
 cd ../devbuild
 open ./ios/project/*.xcodeproj
