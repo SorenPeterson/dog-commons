7126745ec7157f844ced0cfcbe9f01c25594f527
Soren Peterson <soren468@gmail.com>
2015-08-02 12:32:12 -0500

    buttons on information page open in inappbrowser

--- app/client/views/mla/mla.js
+++ app/client/views/mla/mla.js
@@ -2,3 +2,12 @@ Router.route('/mla', function() {
 	Session.set('pageTitle', 'Information');
 	this.render('MLA');
 });
+
+Template.MLA.events({
+	'click a': function(evt, tmpl) {
+		evt.preventDefault();
+		var href = evt.target.closest('a').href;
+		window.open(href, '_blank', 'location=yes');
+	}
+});
+
