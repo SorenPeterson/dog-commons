0e7f23213d658a8618a339ef2c9142a73326f664
Soren Peterson <soren.t.peterson@gmail.com>
2015-08-03 17:44:20 -0500

    wip pedometr

--- app/client/views/pedometer/pedometer.js
+++ app/client/views/pedometer/pedometer.js
@@ -1,10 +1,11 @@
 Template.Pedometer.events({
 	'click .btn.pedometer-start': function() {
-		pedometer.startPedometerUpdates(function(something) {
-			alert(something);
-		},
-		function(error) {
-			alert('error: ' + error);
-		});
+		GeolocationBG.start();
+	}
+});
+
+Template.Pedometer.helpers({
+	logItem: function() {
+		return GeolocationLog.find();
 	}
 });
