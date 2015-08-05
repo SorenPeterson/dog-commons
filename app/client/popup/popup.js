f0c78c75e99edc7ac604ad562618c483ec579f24
Soren Peterson <soren468@gmail.com>
2015-08-05 11:24:44 -0500

    fully functional popup system

---
+++ app/client/popup/popup.js
@@ -0,0 +1,27 @@
+Router.route('/popuptest');
+
+var Popup = function(id) {
+	this.close = function() {
+	};
+	this.hideForever = function() {
+	};
+};
+
+Template.Popup.onRendered(function() {
+	this.popupData = {};
+});
+
+Template.Popup.helpers({
+	hidden: function() {
+		return Session.get('hide-popup-msg-' + this.id);
+	}
+});
+
+Template.Popup.events({
+	'click .button.close': function(evt, tmpl) {
+		Session.set('hide-popup-msg-' + tmpl.data.id, true);
+	},
+	'click .button.dontShow': function(evt, tmpl) {
+		Session.setPersistent('hide-popup-msg-' + tmpl.data.id, true);
+	}
+});
