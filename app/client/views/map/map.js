6e8611a25d226f47049e0a994733223ebf6137ba
Soren Peterson <soren468@gmail.com>
2015-08-05 18:29:01 -0500

    Users location is placed on map

--- app/client/views/map/map.js
+++ app/client/views/map/map.js
@@ -1,15 +1,37 @@
+window.map = undefined;
+
 Template.Map.onRendered(function() {
 	$('#map').height(window.innerHeight - 60);
+});
+
+
+Meteor.startup(function() {
+	var marker;
+	Tracker.autorun(function() {
+		if(Mapbox.loaded()) {
+			marker ? marker.remove : 1;
+			var currentLocation = Geolocation.latLng();
+			if(currentLocation) {
+				marker = L.marker([currentLocation.lat, currentLocation.lng], {}).addTo(map);
+				console.log(marker);
+				console.log(currentLocation);
+			}
+		}
+	});
 });
 
 Template.Map.onRendered(function() {
 	this.autorun(function() {
 		if(Mapbox.loaded()) {
-			L.mapbox.accessToken = 'pk.eyJ1Ijoic29yZW40NjgiLCJhIjoiOTZiMTc3MThlZjFlMTFmOGQ0NjIwNzcwODJhZTM4YWQifQ.Ycl3ox-mvLD4IM64nGNCxA';
-			window.map = L.mapbox.map('map', 'soren468.774e7f1d');
-			var center = new L.LatLng(44.859180, -93.614343);
-			window.map.setView(center, 14);
+			mapboxOnLoaded();
 		}
 	});
 });
+
+var mapboxOnLoaded = function() {
+	L.mapbox.accessToken = 'pk.eyJ1Ijoic29yZW40NjgiLCJhIjoiOTZiMTc3MThlZjFlMTFmOGQ0NjIwNzcwODJhZTM4YWQifQ.Ycl3ox-mvLD4IM64nGNCxA';
+	map = L.mapbox.map('map', 'soren468.2db88a2d');
+	var center = new L.LatLng(44.859180, -93.614343);
+	map.setView(center, 14);
+};
 
