1bc3d6897f1d84b264c7b35a6111d03ab74474ce
Soren Peterson <soren.t.peterson@gmail.com>
2015-08-06 14:12:56 -0500

    remove unnecessary bggeoloc code

--- app/client/main.js
+++ app/client/main.js
@@ -1,31 +1,4 @@
-window.Observations = new Ground.Collection('observations', {connection: null});
-
 Meteor.startup(function() {
 	Mapbox.load();
-
-	Meteor.subscribe('GeolocationLog');
-		
-	if(Meteor.isCordova) {
-		GeolocationBG.config({
-			url: 'http://nbt.otterhive.com/api/geolocation',
-			params: {
-			},
-			headers: {
-			},
-			desiredAccuracy: 10,
-			stationaryRadius: 20,
-			distanceFilter: 30,
-			notificationTitle: 'BackgroundGPS',
-			notificationsText: 'ENABLED',
-			activityType: 'Automotive Navigation',
-			debug: false
-		});
-
-		GeolocationBG.bgGeo.configure(function(location) {
-			GeolocationLog.insert(location);
-		}, function(error) {
-			console.log('error: ', error);
-		}, GeolocationBG.options);
-	}
 });
 
