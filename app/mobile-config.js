11b077acd75853ed5a5738872e9461734cbea19a
Soren Peterson <soren468@gmail.com>
2015-08-06 12:25:15 -0500

    include all icon sizes

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -7,7 +7,13 @@ App.info({
 App.icons({
 	'iphone': 'icons/icon-57.png',
 	'iphone_2x': 'icons/icon-120.png',
-	'iphone_3x': 'icons/icon-180.png'
+	'iphone_3x': 'icons/icon-180.png',
+	'ipad': 'icons/icon-76.png',
+	'ipad_2x': 'icons/icon-152.png',
+	'android_ldpi': 'icons/icon-36.png',
+	'android_mdpi': 'icons/icon-48.png',
+	'android_hdpi': 'icons/icon-72.png',
+	'android_xhdpi': 'icons/icon-96.png',
 });
 
 /*App.accessRule('nbt.otterhive.com/*');
