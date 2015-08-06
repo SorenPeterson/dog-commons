05a39b06038cbe9005f047fca178d4b6859d0cc9
Soren Peterson <soren468@gmail.com>
2015-08-06 14:01:04 -0500

    all launch screens are set up

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -16,11 +16,29 @@ App.icons({
 	'android_xhdpi': 'icons/icon-96.png',
 });
 
+App.launchScreens({
+	// iOS
+	'iphone': 'icons/iphone/Default.png',
+	'iphone_2x': 'icons/iphone/Default@2x.png',
+	'iphone5': 'icons/iphone/Default-568h@2x.png',
+	'iphone6': 'icons/iphone/Default-667h@2x.png',
+	'iphone6p_portrait': 'icons/iphone/Default-Portrait-736h@3x.png',
+
+	'ipad_portrait': 'icons/iphone/Default-Portrait.png',
+	'ipad_portrait_2x': 'icons/iphone/Default-Portrait@2x.png',
+
+	// Android
+	'android_ldpi_portrait': 'icons/android/ldpi.png',
+	'android_mdpi_portrait': 'icons/android/mdpi.png',
+	'android_hdpi_portrait': 'icons/android/hdpi.png',
+	'android_xhdpi_portrait': 'icons/android/xhdpi.png'
+});
+
 /*App.accessRule('nbt.otterhive.com/*');
-App.accessRule('192.168.1.131/*');
-App.accessRule('localhost/*');
-App.accessRule('*.mapbox.com/*');
-App.accessRule('*.audubon.org/*');*/
+  App.accessRule('192.168.1.131/*');
+  App.accessRule('localhost/*');
+  App.accessRule('*.mapbox.com/*');
+  App.accessRule('*.audubon.org/*');*/
 App.accessRule('*');
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
