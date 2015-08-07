ed424fee059acb4764133cbb11a73df52a9fa70e
Soren Peterson <soren468@gmail.com>
2015-08-07 09:11:10 -0500

    needs testing: open u of m links in system browser

--- app/client/main.js
+++ app/client/main.js
@@ -34,6 +34,11 @@ Template.Layout.events({
 		evt.preventDefault();
 		var href = $(evt.target).closest('a').attr('href');
 		window.open(href, '_blank', 'location=yes');
+	},
+	'click a.system-browser': function(evt, tmpl) {
+		evt.preventDefault();
+		var href = $(evt.target).closest('a').attr('href');
+		window.open(href, '_system', 'location=yes');
 	}
 });
 
