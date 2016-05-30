ceb7289aa60a601419cd2edbb6cdab5537505ac3
Soren Peterson <soren468@gmail.com>
2016-05-30 11:35:48 -0500

    modify the build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git pull origin master
 echo y | rm -rf devbuild/*
 cd app
-meteor build ../devbuild --server=http://0.0.0.0:80/
+meteor build ../devbuild --server=http://dog-commons.otterhive.com:80/
 cd ../devbuild
 open ./ios/project/*.xcodeproj
