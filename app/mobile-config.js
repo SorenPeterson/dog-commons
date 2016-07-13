803173803520f83bf070c7bdb5e8b42ed4ed4f84
Soren Peterson <soren468@gmail.com>
2016-07-12 21:17:29 -0500

    the app is DEPLOYEEEDDDDD

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -5,12 +5,10 @@ App.info({
 });
 
 App.icons({
-	'iphone': 'icons/icon-57.png',
 	'iphone_2x': 'icons/icon-120.png',
 	'iphone_3x': 'icons/icon-180.png',
 	'ipad': 'icons/icon-76.png',
 	'ipad_2x': 'icons/icon-152.png',
-	'android_ldpi': 'icons/icon-36.png',
 	'android_mdpi': 'icons/icon-48.png',
 	'android_hdpi': 'icons/icon-72.png',
 	'android_xhdpi': 'icons/icon-96.png',
@@ -18,7 +16,6 @@ App.icons({
 
 App.launchScreens({
 	// iOS
-	'iphone': 'icons/iphone/Default.png',
 	'iphone_2x': 'icons/iphone/Default@2x.png',
 	'iphone5': 'icons/iphone/Default-568h@2x.png',
 	'iphone6': 'icons/iphone/Default-667h@2x.png',
@@ -28,7 +25,6 @@ App.launchScreens({
 	'ipad_portrait_2x': 'icons/iphone/Default-Portrait@2x.png',
 
 	// Android
-	'android_ldpi_portrait': 'icons/android/ldpi.png',
 	'android_mdpi_portrait': 'icons/android/mdpi.png',
 	'android_hdpi_portrait': 'icons/android/hdpi.png',
 	'android_xhdpi_portrait': 'icons/android/xhdpi.png'
